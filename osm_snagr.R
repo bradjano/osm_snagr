97e8d93b0100059eddf23ec1886c53e6393aec8b
Brad Janocha <bradjanocha@gmail.com>
2021-03-12 20:40:23 -0500

    Some lines have been added to the R file

--- osm_snagr.R
+++ osm_snagr.R
@@ -1,1 +1,17 @@
 library("tidyverse")
+library("osmdata")
+
+# First, we need to create a bounding box to select our OSM data.
+
+boxLeft <- 
+boxTop <- 
+boxRight <- 
+boxBottom <- 
+
+myBox <- c()
+
+# Now, we'll create our query.
+
+testQuery <- opq(bbox = myBox) %>%
+  add_osm_feature() %>%
+  osmdata_sf()
