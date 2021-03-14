34eeba832a56dac4e1eca9ad0eb6ab0b1c23d5a1
Brad Janocha <bradjanocha@gmail.com>
2021-03-14 17:00:28 -0400

    Replaces ggmap functions with leaflet

--- osm_snagr.R
+++ osm_snagr.R
@@ -4,11 +4,13 @@ if(!require("tidyverse")) install.packages("tidyverse")
 if(!require("osmdata")) install.packages("osmdata")
 if(!require("sf")) install.packages("sf")
 if(!require("ggmap")) install.packages("ggmap")
+if(!require("leaflet")) install.packages("leaflet")
 
 library("tidyverse")
 library("osmdata")
 library("sf")
 library("ggmap")
+library("leaflet")
 
 
 # Now, we'll create our query.
@@ -22,21 +24,26 @@ q <- getbb(loc) %>%
 d <- osmdata_sf(q)
 
 
-# Building a map to check our query results
+# Building a map to visually inspect our query results
 
 bmap <- get_map(getbb(loc), maptype = "toner-background")
 
-ggmap(bmap) +
-  geom_sf(data = d$osm_points,
-    inherit.aes = FALSE,
-    colour = "#238443",
-    fill = "#004529",
-    alpha = .5,
-    size = 4,
-    shape = 21
-  ) +
-  labs(x = "", y = "")
+# ggmap(bmap) +
+#   geom_sf(data = d$osm_points,
+#     inherit.aes = FALSE,
+#     colour = "#238443",
+#     fill = "#004529",
+#     alpha = .5,
+#     size = 4,
+#     shape = 21
+#   ) +
+#   labs(x = "", y = "")
 
+m <- leaflet(data = d$osm_points) %>%
+  addTiles() %>%
+  addMarkers()
+
+m
 
 # Exporting the data for use in a GIS
 
