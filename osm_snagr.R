f2aec084fab91a90cec001f67c46b647793b2884
Brad Janocha <bradjanocha@gmail.com>
2021-03-14 17:04:10 -0400

    Added cluster options to the leaflet markers

--- osm_snagr.R
+++ osm_snagr.R
@@ -3,13 +3,11 @@
 if(!require("tidyverse")) install.packages("tidyverse")
 if(!require("osmdata")) install.packages("osmdata")
 if(!require("sf")) install.packages("sf")
-if(!require("ggmap")) install.packages("ggmap")
 if(!require("leaflet")) install.packages("leaflet")
 
 library("tidyverse")
 library("osmdata")
 library("sf")
-library("ggmap")
 library("leaflet")
 
 
@@ -26,24 +24,12 @@ d <- osmdata_sf(q)
 
 # Building a map to visually inspect our query results
 
-bmap <- get_map(getbb(loc), maptype = "toner-background")
-
-# ggmap(bmap) +
-#   geom_sf(data = d$osm_points,
-#     inherit.aes = FALSE,
-#     colour = "#238443",
-#     fill = "#004529",
-#     alpha = .5,
-#     size = 4,
-#     shape = 21
-#   ) +
-#   labs(x = "", y = "")
-
 m <- leaflet(data = d$osm_points) %>%
   addTiles() %>%
-  addMarkers()
+  addMarkers(clusterOptions = markerClusterOptions())
 
 m
+
 
 # Exporting the data for use in a GIS
 
